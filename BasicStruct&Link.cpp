bc0d892d61c112ee36447e8c2a14a39c0d280d1c
Fahim Shahriar Eram <78979590+fasher7@users.noreply.github.com>
2021-03-28 13:41:06 +0600

    Create BasicStruct&Link.cpp

---
+++ BasicStruct&Link.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int data = 0;
+};
+
+int main()
+{
+    node* a = nullptr;
+    a = new node;
+    a -> data = 123;
+
+    cout << a << endl; 
+    cout << a -> data << endl;
+
+    return 0;
+}
