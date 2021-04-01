372c4311d251e14bffa31b2cc6f309579b8887da
Fahim Shahriar Eram <78979590+fasher7@users.noreply.github.com>
2021-04-01 13:44:57 +0600

    Create removeLinkList.cpp

---
+++ removeLinkList.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+struct flower
+{
+    int price = 10;
+    flower* nextFlower = nullptr;
+};
+
+int main()
+{
+    flower* rose = new flower();
+    rose -> price = 10;
+
+    flower* tulip = new flower();
+    tulip -> price = 20;
+
+    flower* sunflower = new flower();
+    sunflower -> price = 30;
+
+    flower* lily = new flower();
+    lily -> price = 40;
+
+    rose -> nextFlower = tulip;
+    tulip -> nextFlower = sunflower;
+    sunflower -> nextFlower = lily;
+
+    cout << "Rose: " << rose << endl;
+    cout << "Tulip: " << tulip << endl;
+    cout << "Sunflower: " << sunflower << endl;
+    cout << "Lily: " << lily << endl;
+
+    flower* f = rose;
+    while (f -> nextFlower != nullptr)
+    {
+        if (f -> nextFlower -> price == 30)  //tulip's next is sunflower
+        {
+            flower *junk = f -> nextFlower; //took sunflower's address
+            f -> nextFlower = f -> nextFlower -> nextFlower; //avoided sunflower; f == lily
+            delete junk;
+        }
+        else
+        {
+            f = f -> nextFlower;
+        }
+    }
+
+    cout << "Rose: " << rose -> price << endl;
+    cout << "Tulip: " << tulip -> price << endl;
+    cout << "Sunflower: " << sunflower -> price << endl; //no value so garbage
+    cout << "Lily: " << lily -> price << endl;
+    
+    cout << rose -> nextFlower << endl;
+    cout << rose -> nextFlower -> nextFlower << endl; 
+    // ^ printed lily's address instead of sunflower as it got deleted
+    // The variable sunflower didn't get deleted but from the list it got deleted
+    return 0;
+}
