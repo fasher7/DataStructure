5df00a6ffe917d2ac8f3b541662f2b111ea0d771
Fahim Shahriar Eram <78979590+fasher7@users.noreply.github.com>
2021-04-04 13:40:49 +0600

    Create singlyLinkList.cpp

---
+++ singlyLinkList.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int val = 0;
+    node* next = nullptr; 
+};
+
+void printList(node* n)
+{
+    while (n != nullptr)
+    {
+        cout << n -> val << " " << n << endl;
+        n = n -> next;
+    }
+}
+
+void addValue(node*& n, int data)
+{
+    if (n == nullptr)
+    {
+        n = new node;
+        cout << "n: " << n << endl;
+        n -> val = data;
+    }
+    else
+    {
+        node* current = n;
+        while (current -> next != nullptr)
+        {
+            current = current -> next;
+        }
+        node* p = new node;
+        p -> val = data;
+        current -> next = p;
+    }
+    return;
+}
+
+int main()
+{
+    // singly link list
+    node* sll = nullptr;
+    addValue(sll, 10); // node*&n = sll
+    cout << sll << endl;
+    addValue(sll, 20);
+    cout << sll << endl;    
+    addValue(sll, 30); 
+    cout << sll << endl;
+    addValue(sll, 40);
+    cout << sll << endl;
+    addValue(sll, 50);  
+    cout << sll << endl; 
+    // sll's address doesn't change at all
+    printList(sll);
+    return 0;   
+}
