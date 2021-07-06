ee40f659be37140fbf6052888bc1b707beb13d65
Fahim Shahriar Eram <78979590+fasher7@users.noreply.github.com>
2021-07-06 15:21:30 +0600

    Create dynamic2DArray.cpp
    It has the use of "pointer to a pointer(**)" which is pointing to a "pointer(*)" that is pointing to a list of elements (in a sense array)

---
+++ dynamic2DArray.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row = 4;
+    int a = 1;
+    int **firstp = new int *[row];
+
+    for (int i = 0; i < row; i++)
+    {
+        firstp[i] = new int[i + 1];
+    }
+
+    for (int y = 0; y < row; y++)
+    {
+        for (int x = 0; x <= y; x++)
+        {
+            firstp[y][x] = a;
+            a++;
+        }
+    }
+
+    for (int y = 0; y < row; y++)
+    {
+        for (int x = 0; x <= y; x++)
+        {
+            cout << firstp[y][x] << " ";
+        }
+        cout << endl;
+    }
+
+    for (int b = 0; b < row; ++b)
+    {
+        delete [] firstp[b];
+    }
+    delete [] firstp;
+
+    return 0;
+}
